7a4d1c92fc56d4ccbd2e1da2359e505124965e5b
Stefano Famà <8192713+famastefano@users.noreply.github.com>
2024-01-29 22:34:37 +0100

    Core: assertion macros now use formatted logs

--- Core/AssertionMacros.hpp
+++ Core/AssertionMacros.hpp
@@ -36,6 +36,19 @@ Core::Private::Logger& LogAssertRef();
 #define IMPL_GENERATE_UNIQUE_NAME_COUNTER(name, line)  IMPL_GENERATE_UNIQUE_NAME_COUNTER2(name, line)
 #define IMPL_GENERATE_UNIQUE_NAME()                    IMPL_GENERATE_UNIQUE_NAME_COUNTER(uniqueVariableName, __COUNTER__)
 
+#if ENABLE_LOGGING
+#    define LOG(Category, Level, format, ...)                                         \
+        Category.log(Level, L"{:%Y-%m-%d %T} {:<8} " L## #Category L" " format L"\n", \
+                     std::chrono::system_clock::now(),                                \
+                     Level __VA_OPT__(, ) __VA_ARGS__)
+#    define IMPL_LOG_ASSERT(Category, Level, format, ...)                    \
+        Category.log(Level, L"{:%Y-%m-%d %T} {:<8} LogAssert " format L"\n", \
+                     std::chrono::system_clock::now(),                       \
+                     Level __VA_OPT__(, ) __VA_ARGS__)
+#else
+#    define LOG(Category, Level, format, ...)
+#endif
+
 #if ENABLE_ASSERT
 namespace Private
 {
@@ -54,12 +67,12 @@ struct RecursiveScopeCounter
 };
 }
 // TODO: assertf should log and start and notify the crash handler
-#    define assertf(condition, ...)                                                                    \
-        {                                                                                              \
-            if(!(condition))                                                                           \
-            {                                                                                          \
-                ::Core::Private::LogAssertRef().log(Core::LogLevel::Fatal __VA_OPT__(, ) __VA_ARGS__); \
-            }                                                                                          \
+#    define assertf(condition, format, ...)                                                                     \
+        {                                                                                                       \
+            if(!(condition))                                                                                    \
+            {                                                                                                   \
+                IMPL_LOG_ASSERT(::Core::Private::LogAssertRef(), ::Core::LogLevel::Fatal, format, __VA_ARGS__); \
+            }                                                                                                   \
         }
 
 #    define IMPL_assertNoReentry(counterName)                                                   \
@@ -88,20 +101,22 @@ struct RecursiveScopeCounter
 #endif
 
 #if ENABLE_EXPECT
-#    define IMPL_expectf(checkName, condition, ...)                                                       \
-        {                                                                                                 \
-            static bool checkName = false;                                                                \
-            if(!checkName && !(condition))                                                                \
-                ::Core::Private::LogAssertRef().log(Core::LogLevel::Warning __VA_OPT__(, ) __VA_ARGS__); \
-            checkName = true;                                                                             \
+#    define IMPL_expectf(checkName, condition, format, ...)                                                       \
+        {                                                                                                         \
+            static bool checkName = false;                                                                        \
+            if(!checkName && !(condition))                                                                        \
+            {                                                                                                     \
+                IMPL_LOG_ASSERT(::Core::Private::LogAssertRef(), ::Core::LogLevel::Warning, format, __VA_ARGS__); \
+            }                                                                                                     \
+            checkName = true;                                                                                     \
         }
 
 #    define expectf(condition, ...) IMPL_expectf(IMPL_GENERATE_UNIQUE_NAME(), (condition), __VA_ARGS__)
 #    define expect(condition)       expectf((condition), L"Expectation failed: " #condition)
-#    define expectAlwaysf(condition, ...)                                                                 \
-        {                                                                                                 \
-            if(!(condition))                                                                              \
-                ::Core::Private::LogAssertRef().log(Core::LogLevel::Warning __VA_OPT__(, ) __VA_ARGS__); \
+#    define expectAlwaysf(condition, format, ...)                                                           \
+        if(!(condition))                                                                                    \
+        {                                                                                                   \
+            IMPL_LOG_ASSERT(::Core::Private::LogAssertRef(), ::Core::LogLevel::Fatal, format, __VA_ARGS__); \
         }
 #    define expectAlways(condition) expectAlwaysf((condition), L"Expectation failed: " #condition)
 #else
@@ -115,31 +130,23 @@ struct RecursiveScopeCounter
 namespace Private
 {
 template<typename CondExpr, typename... Args>
-bool verifyfImpl(CondExpr&& cndExpr, wchar_t const* format, Args&&... args)
+bool verifyfImpl(::Core::LogLevel level, CondExpr&& cndExpr, wchar_t const* format, Args&&... args)
 {
     static_assert(std::is_convertible_v<CondExpr, bool>, L"verify/verifyf can't convert the expression to bool");
     bool condition = static_cast<bool>(cndExpr);
     if(!condition)
     {
-        ::Core::Private::LogAssertRef().log(Core::LogLevel::Warning, format, std::forward<Args>(args)...);
+        ::Core::Private::LogAssertRef().log(level, format, std::forward<Args>(args)...);
     }
     return condition;
 }
 }
-#    define verifyf(condition, format, ...) Private::verifyfImpl((condition), format __VA_OPT__(, ) __VA_ARGS__)
-#    define verify(condition)               verifyf((condition), L"Verification failed: " #condition)
+#    define verifyf(condition, format, ...) \
+        Private::verifyfImpl(::Core::LogLevel::Warning, (condition), L"{:%Y-%m-%d %T} {:<8} LogAssert " format L"\n", std::chrono::system_clock::now(), ::Core::LogLevel::Warning __VA_OPT__(, ) __VA_ARGS__)
+#    define verify(condition) verifyf((condition), L"Verification failed: " #condition)
 #else
 #    define verify(condition)               condition
 #    define verifyf(condition, format, ...) condition
-#endif
-
-#if ENABLE_LOGGING
-#    define LOG(Category, Level, format, ...)                                     \
-        Category.log(Level, L"{:%Y-%m-%d %T} " #Category L" {:<8} " format L"\n", \
-                     std::chrono::system_clock::now(),                            \
-                     Level __VA_OPT__(, ) __VA_ARGS__)
-#else
-#    define LOG(Category, Level, format, ...)
 #endif
 
 #define unimplemented() assertf(false, L"Assertion failed: called unimplemented function.")
