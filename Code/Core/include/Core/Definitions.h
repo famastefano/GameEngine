6dc91f85c011fa2250886b7e48c62f5b635ca2ef
Stefano Famà <8192713+famastefano@users.noreply.github.com>
2024-10-20 13:25:36 +0200

    Core - Definitions - Type aliases are now globally accessible

--- Code/Core/include/Core/Definitions.h
+++ Code/Core/include/Core/Definitions.h
@@ -1,7 +1,16 @@
 #pragma once
 
-namespace Core
-{
+#undef u8
+#undef u16
+#undef u32
+#undef u64
+#undef i8
+#undef i16
+#undef i32
+#undef i64
+#undef f32
+#undef f64
+
 using u8  = unsigned char;
 using u16 = unsigned short;
 using u32 = unsigned int;
@@ -14,4 +23,3 @@ using i64 = signed long long;
 
 using f32 = float;
 using f64 = double;
-} // namespace Core
