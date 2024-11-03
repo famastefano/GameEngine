25563dc20b0f1b377fd875f00a04bc64622e2e92
famastefano <8192713+famastefano@users.noreply.github.com>
2024-11-03 18:46:34 +0100

    Core - StringView - Fixed compilation errors

--- Code/Core/include/Core/Container/String.h
+++ Code/Core/include/Core/Container/String.h
@@ -6,6 +6,7 @@
 #include <Core/Container/Vector.h>
 #include <Core/Definitions.h>
 #include <cstring>
+#include <memory>
 #include <type_traits>
 #include <wchar.h>
 
@@ -17,6 +18,8 @@ class StringView : public Span<CharT, DynamicSize>
   static_assert(std::same_as<CharT, char> || std::same_as<CharT, wchar_t>, "StringView supports only char and wchar_t.");
 
 public:
+  using Super = Span<CharT, DynamicSize>;
+
   inline static constexpr i32 NotFound = -1;
 
   using Span<CharT, DynamicSize>::Span;
@@ -37,40 +40,40 @@ public:
 
   constexpr CharT& Front()
   {
-    return operator[](0);
+    return (*this)[0];
   }
 
   constexpr CharT const& Front() const
   {
-    return operator[](0);
+    return (*this)[0];
   }
 
   constexpr CharT& Back()
   {
-    constexpr i32 sz = Size();
-    return operator[](sz > 0 ? sz - 1 : 0);
+    constexpr i32 sz = Super::Size();
+    return (*this)[sz > 0 ? sz - 1 : 0];
   }
 
   constexpr CharT const& Back() const
   {
-    constexpr i32 sz = Size();
-    return operator[](sz > 0 ? sz - 1 : 0);
+    constexpr i32 sz = Super::Size();
+    return (*this)[sz > 0 ? sz - 1 : 0];
   }
 
   constexpr StringView RemovePrefix(i32 const length)
   {
-    return First(length);
+    return Super::First(length);
   }
 
   constexpr StringView RemoveSuffix(i32 const length)
   {
-    return First(Size() - length);
+    return Super::First(Super::Size() - length);
   }
 
   constexpr i32 Copy(CharT* dest, i32 const count, i32 const offset = 0) const
   {
-    CharT*    src         = &operator[](offset);
-    i32 const maximumCopy = Size() - offset;
+    CharT*    src         = std::addressof((*this)[offset]);
+    i32 const maximumCopy = Super::Size() - offset;
     i32 const actualCount = maximumCopy < count ? maximumCopy : count;
     Algorithm::Copy(src, src + actualCount, dest);
     return actualCount;
@@ -78,110 +81,110 @@ public:
 
   constexpr StringView SubStr(i32 const offset, i32 const count) const
   {
-    return SubSpan(offset, count);
+    return Super::SubSpan(offset, count);
   }
 
   constexpr bool StartsWith(StringView const other) const
   {
-    if (Size() < other.Size() || other.IsEmpty())
+    if (Super::Size() < other.Size() || other.IsEmpty())
       return false;
-    return memcmp(Data(), other.Data(), other.AllocSize()) == 0;
+    return memcmp(Super::Data(), other.Data(), other.AllocSize()) == 0;
   }
 
   constexpr bool StartsWith(CharT const c) const
   {
-    return !IsEmpty() && Front() == c;
+    return !Super::IsEmpty() && Super::Front() == c;
   }
 
   constexpr bool EndsWith(StringView const other) const
   {
-    if (Size() < other.Size() || other.IsEmpty())
+    if (Super::Size() < other.Size() || other.IsEmpty())
       return false;
-    return memcmp(Data() + Size() - other.Size(), other.Data(), other.AllocSize()) == 0;
+    return memcmp(Super::Data() + Super::Size() - other.Size(), other.Data(), other.AllocSize()) == 0;
   }
 
   constexpr bool EndsWith(CharT const c) const
   {
-    return !IsEmpty() && Back() == c;
+    return !Super::IsEmpty() && Super::Back() == c;
   }
 
   constexpr bool Contains(CharT const c) const
   {
-    return Find(c);
+    return Super::Find(c);
   }
 
   constexpr bool Contains(StringView const other)
   {
-    if (Size() < other.Size() || IsEmpty() || other.IsEmpty())
+    if (Super::Size() < other.Size() || Super::IsEmpty() || other.IsEmpty())
       return false;
 
     CharT* p;
     if constexpr (std::same_as<CharT, char>)
     {
-      p = memchr(Data(), other[0], Size());
+      p = memchr(Super::Data(), other[0], Super::Size());
     }
     else
     {
-      p = wmemchr(Data(), other[0], Size());
+      p = wmemchr(Super::Data(), other[0], Super::Size());
     }
 
-    if (p + other.Size() < Data() + Size())
+    if (p + other.Size() < Super::Data() + Super::Size())
       return memcmp(p, other.Data(), other.AllocSize()) == 0;
     return false;
   }
 
   constexpr i32 Find(CharT const c, i32 const offset = 0) const
   {
-    if (IsEmpty() || u32(offset) >= u32(Size()))
+    if (Super::IsEmpty() || u32(offset) >= u32(Super::Size()))
       return NotFound;
 
     CharT* p;
     if constexpr (std::same_as<CharT, char>)
     {
-      p = memchr(Data() + offset, c, Size());
+      p = memchr(Super::Data() + offset, c, Super::Size());
     }
     else
     {
-      p = wmemchr(Data() + offset, c, Size())
+      p = wmemchr(Super::Data() + offset, c, Super::Size());
     }
-    return p ? p - Data() : NotFound;
+    return p ? p - Super::Data() : NotFound;
   }
 
   constexpr i32 Find(StringView const other, i32 const offset = 0) const
   {
-    if (IsEmpty() || u32(offset) >= u32(Size()) || u32(Size()) < other.Size() + u32(offset))
+    if (Super::IsEmpty() || u32(offset) >= u32(Super::Size()) || u32(Super::Size()) < other.Size() + u32(offset))
       return NotFound;
 
     CharT* p;
     if constexpr (std::same_as<CharT, char>)
     {
-      p = memchr(Data() + offset, c, Size());
+      p = memchr(Super::Data() + offset, other[0], Super::Size());
     }
     else
     {
-      p = wmemchr(Data() + offset, c, Size())
+      p = wmemchr(Super::Data() + offset, other[0], Super::Size());
     }
     if (p && memcmp(p, other.Data(), other.AllocSize()) == 0)
-      return p - Data();
+      return p - Super::Data();
     return NotFound;
   }
 
   constexpr i32 FindFirstOf(CharT const c, i32 const offset = 0) const
   {
-    return Find(c, offset);
+    return Super::Find(c, offset);
   }
 
   constexpr i32 FindFirstOf(StringView const other, i32 const offset = 0) const
   {
-    return Find(other, offset);
+    return Super::Find(other, offset);
   }
 
   constexpr i32 FindLastOf(CharT const c, i32 const offset = 0) const
   {
-    if (IsEmpty() || u32(Size()) <= u32(offset))
+    if (Super::IsEmpty() || u32(Super::Size()) <= u32(offset))
       return NotFound;
 
-    for (i32 i = Size() - 1; i >= offset; --i)
+    for (i32 i = Super::Size() - 1; i >= offset; --i)
     {
       if (operator[](i) == c)
         return i;
@@ -191,12 +194,12 @@ public:
 
   constexpr i32 FindLastOf(StringView const other, i32 const offset = 0) const
   {
-    if (IsEmpty() || other.IsEmpty() || u32(Size()) < u32(other.Size() + offset))
+    if (Super::IsEmpty() || other.IsEmpty() || u32(Super::Size()) < u32(other.Size() + offset))
       return NotFound;
-       
-    for (i32 i = Size() - other.Size() - 1; i >= offset; --i)
+
+    for (i32 i = Super::Size() - other.Size() - 1; i >= offset; --i)
     {
-      if (operator[](i) == c && memcmp(Data() + i, other.Data(), other.AllocSize()) == 0)
+      if ((*this)[i] == other[i] && memcmp(Super::Data() + i, other.Data(), other.AllocSize()) == 0)
         return i;
     }
     return NotFound;
@@ -224,7 +227,7 @@ public:
 
   constexpr bool operator==(StringView const other) const
   {
-    return Size() == other.Size() && memcmp(Data(), other.Data(), AllocSize()) == 0;
+    return Super::Size() == other.Size() && memcmp(Super::Data(), other.Data(), Super::AllocSize()) == 0;
   }
 
   constexpr bool operator!=(StringView const other) const
@@ -234,16 +237,16 @@ public:
 
   constexpr bool operator<(StringView const other) const
   {
-    if (IsEmpty() && other.IsEmpty())
+    if (Super::IsEmpty() && other.IsEmpty())
       return false;
 
-    if (IsEmpty())
+    if (Super::IsEmpty())
       return true;
 
     if (other.IsEmpty())
       return false;
 
-    return memcmp(Data(), other.Data(), AllocSize())
+    return memcmp(Super::Data(), other.Data(), Super::AllocSize());
   }
 };
 } // namespace Core
---
+++ Code/CoreTests/src/Container/TestStringView.cpp
@@ -0,0 +1,7 @@
+#include <Core/Container/String.h>
+#include <UnitTest/UnitTest.h>
+
+UNIT_TEST_SUITE(StringView)
+{
+
+}
