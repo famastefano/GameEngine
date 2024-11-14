08737c4356a242e6dd384ac27c71b7bbdddd0fcf
famastefano <8192713+famastefano@users.noreply.github.com>
2024-11-15 00:30:19 +0100

    Core - Assert - Integrated with new logging system

--- Code/Core/include/Core/Assert/Assert.h
+++ Code/Core/include/Core/Assert/Assert.h
@@ -1,9 +1,26 @@
 #pragma once
 
-// TODO: Add proper configurable logging macros + minidump + msg fmt
+#include <Core/Logging/Logging.h>
+#include <Core/Platform/Platform.h>
+
+GE_DEFINE_INLINE_LOG_CATEGORY(LogAssert, Core::Verbosity::Warning)
+
+// TODO: Add minidump on check(false) + msg fmt
 
 #undef assert
 
-#define check(cnd, ...) if(!(cnd)) __debugbreak()
+#define check(cnd, ...)                                                                               \
+  if (!(cnd))                                                                                         \
+  {                                                                                                   \
+    GE_LOG(LogAssert, Core::Verbosity::Error, "Check failed: `" #cnd "`" __VA_OPT__(, ) __VA_ARGS__); \
+    if (Core::IsDebuggerAttached())                                                                   \
+      __debugbreak();                                                                                 \
+  }
+
+#define verify(cnd, ...) [](const bool v) {                                                              \
+  if (!v)                                                                                                \
+    GE_LOG(LogAssert, Core::Verbosity::Warning, "Verify failed: `" #cnd "`" __VA_OPT__(, ) __VA_ARGS__); \
+  return v;                                                                                              \
+}((cnd))
 
 #define assert(...) check(__VA_ARGS__)
