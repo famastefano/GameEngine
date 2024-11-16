2a2d8f07d8d77acbd14b44311a3d1d0a5bc4bbf0
famastefano <8192713+famastefano@users.noreply.github.com>
2024-11-16 17:03:28 +0100

    Core - Assert - Removed in release build

--- Code/Core/include/Core/Assert/Assert.h
+++ Code/Core/include/Core/Assert/Assert.h
@@ -8,30 +8,46 @@ GE_DEFINE_INLINE_LOG_CATEGORY(LogAssert, Core::Verbosity::Warning)
 // TODO: Add minidump on check(false) + msg fmt
 
 #undef assert
+#undef checkf
+#undef check
+#undef verifyf
+#undef verify
+#undef verifyOncef
+#undef verifyOnce
 
-#define checkf(Condition, Format, ...)                                                                              \
-  if (!(Condition))                                                                                                 \
-  {                                                                                                                 \
-    GE_LOG(LogAssert, Core::Verbosity::Error, "Check failed: `" #Condition "` " Format __VA_OPT__(, ) __VA_ARGS__); \
-    if (Core::IsDebuggerAttached())                                                                                 \
-      __debugbreak();                                                                                               \
-  }
-#define check(Condition) checkf(Condition, "")
+#ifdef GE_BUILD_CONFIG_RELEASE
+#  define assert(...)
+#  define checkf(...)
+#  define check(...)
+#  define verifyf(...)
+#  define verify(...)
+#  define verifyOncef(...)
+#  define verifyOnce(...)
+#else
+#  define checkf(Condition, Format, ...)                                                                              \
+    if (!(Condition))                                                                                                 \
+    {                                                                                                                 \
+      GE_LOG(LogAssert, Core::Verbosity::Error, "Check failed: `" #Condition "` " Format __VA_OPT__(, ) __VA_ARGS__); \
+      if (Core::IsDebuggerAttached())                                                                                 \
+        __debugbreak();                                                                                               \
+    }
+#  define check(Condition) checkf(Condition, "")
 
-#define verifyf(Condition, Format, ...) [](const bool v) {                                                             \
-  if (!v)                                                                                                              \
-    GE_LOG(LogAssert, Core::Verbosity::Warning, "Verify failed: `" #Condition "` " Format __VA_OPT__(, ) __VA_ARGS__); \
-  return v;                                                                                                            \
-}((Condition))
-#define verify(Condition) verifyf(Condition, "")
+#  define verifyf(Condition, Format, ...) [](const bool v) {                                                             \
+    if (!v)                                                                                                              \
+      GE_LOG(LogAssert, Core::Verbosity::Warning, "Verify failed: `" #Condition "` " Format __VA_OPT__(, ) __VA_ARGS__); \
+    return v;                                                                                                            \
+  }((Condition))
+#  define verify(Condition) verifyf(Condition, "")
 
-#define verifyOncef(Condition, Format, ...) [](const bool v) {                                                         \
-  static bool alreadyVerified = false;                                                                                 \
-  if (!v && !alreadyVerified)                                                                                          \
-    GE_LOG(LogAssert, Core::Verbosity::Warning, "Verify failed: `" #Condition "` " Format __VA_OPT__(, ) __VA_ARGS__); \
-  alreadyVerified = true;                                                                                              \
-  return v;                                                                                                            \
-}((Condition))
-#define verifyOnce(Condition) verifyOncef(Condition, "")
+#  define verifyOncef(Condition, Format, ...) [](const bool v) {                                                         \
+    static bool alreadyVerified = false;                                                                                 \
+    if (!v && !alreadyVerified)                                                                                          \
+      GE_LOG(LogAssert, Core::Verbosity::Warning, "Verify failed: `" #Condition "` " Format __VA_OPT__(, ) __VA_ARGS__); \
+    alreadyVerified = true;                                                                                              \
+    return v;                                                                                                            \
+  }((Condition))
+#  define verifyOnce(Condition) verifyOncef(Condition, "")
 
-#define assert(Condition) check(Condition)
+#  define assert(Condition) check(Condition)
+#endif
