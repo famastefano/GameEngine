298996e62565c7e1c2973fd993381920ebfe6557
famastefano <8192713+famastefano@users.noreply.github.com>
2024-11-20 22:32:40 +0100

    Core - Hash - Added support for function pointers and methods

--- Code/Core/include/Core/Hash/Hash.h
+++ Code/Core/include/Core/Hash/Hash.h
@@ -1,9 +1,27 @@
 #pragma once
 
-#include <Core/Definitions.h>
 #include <Core/API.h>
+#include <Core/Definitions.h>
 
 namespace Core
 {
 CORE_API u64 CalculateHash(void const* p, i32 const bytes);
+
+template <typename R, typename... Args>
+u64 CalculateHash(R (*Func)(Args...))
+{
+  return Func;
 }
+
+template <typename T, typename R, typename... Args>
+u64 CalculateHash(T* Instance, R (T::*Method)(Args...))
+{
+#pragma pack(1)
+  struct Dummy
+  {
+    T*               Instance_ = Instance;
+    decltype(Method) Method_   = Method;
+  } Dummy_;
+  return CalculateHash(&Dummy_, sizeof(Dummy_));
+}
+} // namespace Core
