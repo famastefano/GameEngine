41f45d59bded450a4014517ed75809f03e883e60
famastefano <8192713+famastefano@users.noreply.github.com>
2024-11-22 20:43:05 +0100

    Core - Input - Fixed compilation under monolithic build

--- Code/Input/include/Input/API.h
+++ Code/Input/include/Input/API.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#if !GE_BUILD_ENABLE_MONOLITHIC
+#if GE_BUILD_ENABLE_MONOLITHIC
+#  define INPUT_API
+#else
 #  ifdef INPUT_API_EXPORTS
 #    define INPUT_API __declspec(dllexport)
 #  else
