fd4a07b4df2c9bacd2eea20d56e8cdc87fcf2486
Stefano Famà <8192713+famastefano@users.noreply.github.com>
2024-12-18 17:17:43 +0100

    Core - Reflection - Implemented minimal reflection

--- Code/Core/include/Core/Reflection/Reflection.h
+++ Code/Core/include/Core/Reflection/Reflection.h
@@ -1,29 +1,54 @@
 #pragma once
 
-#undef GAME_ENGINE_RTTI
+#include <Core/Definitions.h>
+#include <concepts>
 
-#define GAME_ENGINE_DEFINE_RTTI(Type)                    \
-public:                                                  \
-  static ::Core::Reflection::RTTI const& GetRTTI()       \
-  {                                                      \
-    static ::Core::Reflection::RTTI rtti{.Name = #Type}; \
-    return rtti;                                         \
-  }                                                      \
-                                                         \
-  template <typename T>                                  \
-  T* Cast()                                              \
-  {                                                      \
-    if (&GetRTTI() == &T::GetRTTI())                     \
-      return static_cast<T*>(this);                      \
-    return nullptr;                                      \
-  }                                                      \
-                                                         \
+namespace Core
+{
+struct TypeMetaData
+{
+  u64         ID_;
+  char const* Name_;
+};
+template <typename T>
+concept Reflectable = requires(T t) {
+  { t.GetTypeMetaData() } -> std::same_as<TypeMetaData const&>;
+  { T::GetStaticTypeMetaData() } -> std::same_as<TypeMetaData const&>;
+};
+} // namespace Core
+
+#define GE_DECLARE_TYPE_METADATA_BASE()                          \
+  virtual const Core::TypeMetaData& GetTypeMetaData() const = 0; \
+                                                                 \
+  template <Core::Reflectable T>                                 \
+  T* GetAs()                                                     \
+  {                                                              \
+    if (GetTypeMetaData().ID_ == T::GetStaticTypeMetaData().ID_) \
+      return static_cast<T*>(this);                              \
+    return nullptr;                                              \
+  }
+
+#define GE_DECLARE_CLASS_TYPE_METADATA()                     \
+public:                                                      \
+  virtual const Core::TypeMetaData& GetTypeMetaData() const; \
+                                                             \
 private:
 
-namespace Core::Reflection
-{
-struct RTTI
-{
-  char const* Name;
-};
-} // namespace Core::Reflection
+#define GE_DECLARE_STRUCT_TYPE_METADATA()                    \
+  virtual const Core::TypeMetaData& GetTypeMetaData() const; \
+  static const Core::TypeMetaData&  GetStaticTypeMetaData();
+
+#define GE_DEFINE_TYPE_METADATA(FullyQualifiedTypeWithNamespace)                     \
+  const Core::TypeMetaData& FullyQualifiedTypeWithNamespace::GetTypeMetaData() const \
+  {                                                                                  \
+    return GetStaticTypeMetaData();                                                  \
+  }                                                                                  \
+  const Core::TypeMetaData& FullyQualifiedTypeWithNamespace::GetStaticTypeMetaData() \
+  {                                                                                  \
+    static const char*        name = #FullyQualifiedTypeWithNamespace;               \
+    static Core::TypeMetaData typeMetaData{                                          \
+        .ID_   = (u64) & name,                                                       \
+        .Name_ = name,                                                               \
+    };                                                                               \
+    return typeMetaData;                                                             \
+  }
