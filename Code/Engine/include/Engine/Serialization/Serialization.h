c26caa52c2837e27c0e3cf8c2f43f2ec3faf98fa
Stefano Famà <8192713+famastefano@users.noreply.github.com>
2024-12-18 22:47:13 +0100

    Saving work

---
+++ Code/Engine/include/Engine/Serialization/Serialization.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <Core/Definitions.h>
+
+namespace Engine::Serialization
+{
+#pragma pack(push, 1)
+struct SerializationHeader
+{
+  u32 Magic_   = ('F' << 24) | ('A' << 16) | ('M' << 8) | 'A';
+  u32 Version_ = 0;
+  u64 TypeID_  = 0;
+  u32 Length_  = 0;
+};
+#pragma pack(pop)
+} // namespace Engine::Serialization
